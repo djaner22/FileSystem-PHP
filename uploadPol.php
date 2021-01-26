c020f1345a9976a3565e36dee982fc3fb463d0b8
xZ1r0 <pgispert23@gmail.com>
2021-01-26 19:42:46 +0100

    upload & index Pol

--- uploadPol.php
+++ uploadPol.php
@@ -1,4 +1,7 @@
-
+<?php
+$file = $_POST['file'];
+$readCsv = array_map('str_getcsv', $file);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -8,22 +11,12 @@
 
 <body>
 <?php
-function arrayFromCSV($file, $hasFieldNames = false, $delimiter = ',', $enclosure='') {
-    $result = Array();
-    $size = filesize($file) +1;
-    $file = fopen($file, 'r');
-    #TO DO: There must be a better way of finding out the size of the longest row... until then
-    if ($hasFieldNames) $keys = fgetcsv($file, $size, $delimiter, $enclosure);
-    while ($row = fgetcsv($file, $size, $delimiter, $enclosure)) {
-        $n = count($row); $res=array();
-        for($i = 0; $i < $n; $i++) {
-            $idx = ($hasFieldNames) ? $keys[$i] : $i;
-            $res[$idx] = $row[i];
-        }
-        $result[] = $res;
+foreach ($readCsv as $itemCsv) {
+    echo "<tr>";
+    foreach ($itemCsv as $elementoItemCSV) {
+        echo "<td>$elementoItemCSV</td>";
     }
-    fclose($file);
-    return $result;
+    echo "</tr>";
 }
 ?>
 </body>
